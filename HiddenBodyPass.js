d4c2ff0c8ed48c575ed0206b8fdfaeb931b22ac5
Júlio Moreira <julio.moreira666@gmail.com>
2016-07-26 17:04:29 -0300

    JS
    JS para inclusão na página que deseja ocultar o Body com senha.

---
+++ HiddenBodyPass.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $(document.body).hide();
+    var password = prompt("Site em construção, digite a senha fornecida!");
+    if (password == "senha_que_deseja")
+    {
+        $(document.body).show();
+    }
+    else
+    {
+        $(document.body).hide();
+        alert("Senha incorreta!");
+    }
+});
